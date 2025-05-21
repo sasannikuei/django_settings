ce1591ef31f5eeefafc5a11111aa70ca8cb18635
Sasan <sasan.nikuei@gmail.com>
2025-05-21 11:33:55 +0330

    The first commit

---
+++ backend/settings/dev.py
@@ -0,0 +1,12 @@
+from .base_settings import *
+
+INSTALLED_APPS += [
+    'base.apps.BaseConfig'
+]
+
+# DEBUG = True
+# ALLOWED_HOSTS = []
+#
+# ROOT_URLCONF = "backend.urls"
+
+print("Loaded dev settings!")
